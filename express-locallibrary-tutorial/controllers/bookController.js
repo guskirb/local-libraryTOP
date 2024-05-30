ba07cfd14a21d3cac2e7a0c204913e67e4452912
guskirb <anguskirby1998@gmail.com>
2024-05-30 22:15:38 +0100

    Add book details controller function & pug files

--- express-locallibrary-tutorial/controllers/bookController.js
+++ express-locallibrary-tutorial/controllers/bookController.js
@@ -35,12 +35,27 @@ exports.book_list = asyncHandler(async (req, res, next) => {
         .sort({ title: 1 })
         .populate('author')
         .exec();
-        
+
     res.render('book_list', { title: 'Book List', book_list: allBooks });
 });
 
 exports.book_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Book detail: ${req.params.id}`);
+    const [book, bookInstances] = await Promise.all([
+        Book.findById(req.params.id).populate('author').populate('genre').exec(),
+        BookInstance.find({ book: req.params.id }).exec(),
+    ]);
+
+    if (book === null){
+        const err = new Error('Book not found!');
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render('book_detail', {
+        title: book.title,
+        book: book,
+        book_instances: bookInstances,
+    });
 });
 
 exports.book_create_get = asyncHandler(async (req, res, next) => {
