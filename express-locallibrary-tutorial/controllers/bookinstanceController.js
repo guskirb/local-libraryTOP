f34101fd0c172ee345faeaecca2d79fd50874c99
guskirb <anguskirby1998@gmail.com>
2024-05-31 23:03:05 +0100

    Add instance delete controller method & pug file

--- express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -80,11 +80,23 @@ exports.bookinstance_create_post = [
 ];
 
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+    const bookInstance = await BookInstance.findById(req.params.id)
+        .populate('book')
+        .exec();
+
+    if (bookInstance === null) {
+        res.redirect('/catalog/bookinstances');
+    }
+
+    res.render('bookinstance_delete', {
+        title: 'Delete Book Instance',
+        bookinstance: bookInstance,
+    })
 });
 
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+    await BookInstance.findByIdAndDelete(req.body.bookinstanceid);
+    res.redirect('/catalog/bookinstances');
 });
 
 exports.bookinstance_update_get = asyncHandler(async (req, res, next) => {
